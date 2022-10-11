fcee6f2177a5dff7d4faffaf0422394c26f496e9
dGakamsky <46941271+dGakamsky@users.noreply.github.com>
2022-10-11 20:54:22 +0100

    Now does a grid display

--- Grid.java
+++ Grid.java
@@ -1,11 +1,15 @@
+import java.util.Random;
+
 public class Grid {
     Tile[][] tileGrid;
     int xDimension;
     int yDimension;
+    int maxMines;
 
-    Grid(int x, int y){
+    Grid(int x, int y, int mines){
         this.xDimension = x;
         this.yDimension = y;
+        this.maxMines = mines;
         this.tileGrid = generaTileGrid(this.xDimension, this.yDimension);
     }
 
@@ -15,13 +19,97 @@ public class Grid {
 
     Tile[][] generaTileGrid (int x, int y){
         tileGrid = new Tile[x][y];
+
         for (int i = 0 ; i < x ; i++) {
             for (int j = 0; j < y ; j++){
-                tileGrid[i][j] = new Tile(this, x, y);
+                tileGrid[i][j] = new Tile( x, y);
 
             }
         }
+        populateMines(x, y);
+        for (int i = 0 ; i < x ; i++) {
+            for (int j = 0; j < y ; j++){
+                tileGrid[i][j].minesAdjacent = getMines(x, y) ;
+            }
+        }
         return tileGrid;
+    }
+
+    int getMines(int x, int y){
+        int numberOfAdjacentMines = 0;
+        int[] xRange = {x - 1, x, x + 1};
+        int[] yRange = {y-1, y, y+1};
+        for (int xIterator : xRange){
+            if (xIterator < xDimension && xIterator>0) {
+                for (int yIterator : yRange) {
+                    if (yIterator < yDimension && yIterator > 0) {
+                        if (this.tileGrid[xIterator][yIterator].mine) {
+                            numberOfAdjacentMines++;
+                        }
+                    }
+                }
+            }
+        }
+        return numberOfAdjacentMines;
+    }
+
+    void revealTile(int x, int y){
+        if (this.tileGrid[x][y].hidden) {
+            this.tileGrid[x][y].hidden = false;
+            /* TODO : add visual change to it when revealed*/
+            if (this.tileGrid[x][y].mine) {
+                this.gameOver();
+            }
+            if (this.tileGrid[x][y].minesAdjacent == 0) {
+                int[] xRange = {x - 1, x, x + 1};
+                int[] yRange = {y - 1, y, y + 1};
+                for (int xIterator : xRange) {
+                    if (xIterator<xDimension && xIterator>0) {
+                        for (int yIterator : yRange) {
+                            if (yIterator < yDimension && yIterator > 0) {
+                                if (this.tileGrid[xIterator][yIterator].mine) {
+                                    revealTile(xIterator, yIterator);
+                                }
+                            }
+                        }
+                    }
+                }
+
+            }
+        }
+
+    }
+
+    void populateMines(int x, int y){
+        int currentMines = 0;
+        Random rand = new Random();
+        while (currentMines < this.maxMines){
+            int randomX = rand.nextInt(x);
+            int randomY = rand.nextInt(y);
+            if (tileGrid[randomX][randomY].mine == false){
+                tileGrid[randomX][randomY].mine = true;
+                currentMines++;
+            }
+        }
+    }
+
+    void printGrid(){
+        int x = this.xDimension;
+        int y = this.yDimension;
+        System.out.println("This is the grid");
+        System.out.println("[----------------------------------------------------------]");
+        for (int i = 0 ; i < x ; i++) {
+            System.out.println("");
+            for (int j = 0; j < y ; j++){
+                if (tileGrid[i][j].mine){
+                    System.out.print("[ 1 ]");
+                } else {
+                    System.out.print("[ 0 ]");
+                }
+            }
+        }
+        System.out.println("");
+        System.out.println("[----------------------------------------------------------]");
     }
 
 
--- Tile.java
+++ Tile.java
@@ -1,12 +1,12 @@
 public class Tile {
-    int mineAdjacent;
-    Grid grid;
+    int minesAdjacent;
+
     boolean mine;
     boolean hidden;
     boolean flag;
     int[] position = new int[2];
 
-    public Tile(Grid grid, int x, int y){
+    public Tile( int x, int y){
 
         this.flag = false;
         this.hidden = false;
@@ -22,22 +22,6 @@ public class Tile {
         }
     }
 
-    void getMines(){
-        int mines = 0;
-        int x = this.position[0];
-        int[] xRange = {x - 1, x, x + 1};
-        int y = this.position[1];
-        int[] yRange = {y-1, y, y+1};
-        for (int xIterator : xRange){
-            for (int yIterator : yRange){
-                if (this.grid.tileGrid[xIterator][yIterator].mine){
-                    mines++;
-                }
-            }
-        }
-        this.mineAdjacent = mines;
-    }
-
     void flagTile(){
         if (this.hidden){
             this.flag = true;
@@ -45,27 +29,6 @@ public class Tile {
         }
 
     }
-
-    void revealTile(){
-        if (this.hidden){
-            this.hidden = false;
-            /* TODO : add visual change to it when revealed*/
-            if (this.mine){
-                this.grid.gameOver();
-            }
-            if (this.mineAdjacent == 0){
-                int x = this.position[0];
-                int[] xRange = {x - 1, x, x + 1};
-                int y = this.position[1];
-                int[] yRange = {y-1, y, y+1};
-                for (int xIterator : xRange){
-                    for (int yIterator : yRange){
-                        this.grid.tileGrid[xIterator][yIterator].revealTile();
-                        }
-                    }
-                }
-            }
-        }
 }
 
 
