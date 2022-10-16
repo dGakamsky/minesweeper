cffbf79c1ec8a75db71026d2ab84bca5dad9f6cd
dGakamsky <46941271+dGakamsky@users.noreply.github.com>
2022-10-16 22:53:59 +0100

    added some more tests, only non tested methods are some getters/setters and the output/input stuff

--- src/test/java/GridTest.java
+++ src/test/java/GridTest.java
@@ -69,14 +69,44 @@ public class GridTest {
 
     @Test
     void testFlagTile(){
-        Grid flagGrid = new Grid(5,5,2);
-        flagGrid.generaTileGrid(5,5);
+        Grid flagGrid = new Grid(1,2,2);
+        flagGrid.generaTileGrid(1,2);
         Assertions.assertEquals(2, flagGrid.getMaxFlags(),"flags were not assigned");
         flagGrid.flagTile(0,0);
         Assertions.assertTrue(flagGrid.tileGrid[0][0].getFlag(),"flag was not placed");
         Assertions.assertEquals(1, flagGrid.getMaxFlags(),"flag was not used");
+        Assertions.assertEquals(1,flagGrid.getDefusedMines(),"mine was not diffused");
         flagGrid.flagTile(0,0);
+        Assertions.assertFalse(flagGrid.tileGrid[0][0].getFlag(),"flag was not removed");
         Assertions.assertEquals(2, flagGrid.getMaxFlags(),"flag was not retrieved");
+        Assertions.assertEquals(0,flagGrid.getDefusedMines(),"mine was not un-diffused");
+
+    }
+
+    @Test
+    void testRevealTile(){
+        Grid revealGrid = new Grid (5,5,0);
+        revealGrid.generaTileGrid(5,5);
+        Assertions.assertTrue(revealGrid.tileGrid[0][0].getHidden(),"tile was not instantiated as hidden");
+        revealGrid.revealTile(0,0);
+        Assertions.assertFalse(revealGrid.tileGrid[0][0].getHidden(),"tile was not revealed");
+        for (int i = 0; i<5; i++){
+            for (int j = 0; j<5; j++){
+                Assertions.assertFalse(revealGrid.tileGrid[i][j].getHidden(),"zero cascade did not occur");
+            }
+        }
+    }
+
+    @Test
+    void testPopulateMines(){
+        Grid mineGrid = new Grid (5,5,25);
+        mineGrid.generaTileGrid(5,5);
+        for (int i = 0; i<5; i++) {
+            for (int j = 0; j < 5; j++) {
+                Assertions.assertTrue(mineGrid.tileGrid[i][j].getMine(), "mines were not set propertly");
+
+            }
+        }
 
     }
 
